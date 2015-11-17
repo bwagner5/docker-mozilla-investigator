09e18f0b81271504c360d6e2e0e65b3cc0757d5c
Brandon Wagner <bmwagner10@gmail.com>
2015-11-17 12:51:11 -0500

    rabbitmq setup and configured with readme for build and run instructions

---
+++ rabbitmq/README.md
@@ -0,0 +1,7 @@
+#RabbitMQ Docker Container for MIG
+
+### RabbitMQ can be built and run in a docker container with the following commands:
+
+`docker build -t mig/rabbitmq`
+
+`docker run -d -h mig.example.com mig/rabbitmq`
---
+++ rabbitmq/create_rabbitmq_config.sh
@@ -0,0 +1,67 @@
+#! /usr/bin/env bash
+
+adminpass=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})
+schedpass=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})
+agentpass=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})
+workrpass=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})
+
+echo "creating rabbitmq users"
+rabbitmqctl add_user admin $adminpass
+rabbitmqctl set_user_tags admin administrator
+rabbitmqctl add_user scheduler $schedpass
+rabbitmqctl add_user agent $agentpass
+rabbitmqctl add_user worker $workrpass
+
+echo "deleting guest user"
+rabbitmqctl delete_user guest
+
+echo "creating 'mig' vhost"
+rabbitmqctl add_vhost mig
+
+echo "creating ACLs for scheduler user"
+rabbitmqctl set_permissions -p mig scheduler \
+        '^(toagents|toschedulers|toworkers|mig\.agt\..*)$' \
+        '^(toagents|toworkers|mig\.agt\.(heartbeats|results))$' \
+	'^(toagents|toschedulers|toworkers|mig\.agt\.(heartbeats|results))$'
+
+echo "creating ACLs for agent user"
+rabbitmqctl set_permissions -p mig agent \
+        '^mig\.agt\..*$' \
+        '^(toschedulers|mig\.agt\..*)$' \
+        '^(toagents|mig\.agt\..*)$'
+
+echo "creating ACLs for worker user"
+rabbitmqctl set_permissions -p mig worker \
+	'^migevent\..*$' \
+	'^migevent(|\..*)$' \
+	'^(toworkers|migevent\..*)$'
+
+
+
+
+       
+        
+                    
+                   
+                     
+                        
+                       
+                      
+                               
+  
+                                 
+                                  
+
+echo "set mirroring policy"
+rabbitmqctl -p mig set_policy mig-mirror-all "^(toschedulers|toagents|toworkers|mig(|event))\." '{"ha-mode":"all"}'
+
+chown rabbitmq /etc/rabbitmq/*
+echo
+echo "rabbitmq configured with the following users:"
+echo "  admin       $adminpass"
+echo "  scheduler   $schedpass"
+echo "  agent       $agentpass"
+echo "  worker      $workrpass"
+echo
+echo "copy ca.crt and rabbitmq.{crt,key} into /etc/rabbitmq/"
+echo "then run $ service rabbitmq-server restart"
